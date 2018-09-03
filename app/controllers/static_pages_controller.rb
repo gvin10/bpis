79d7cfc00889b725ec920c10ce0c60a19dfebd70
Gregorio Vincent <gv46@cornell.edu>
2018-09-03 22:19:34 +0000

    finish footer & add other pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,19 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def about
+  end
+
+  def events
+  end
+
+  def gallery
+  end
+
+  def joinourteam
+  end
+
+  def donate
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  get 'events', to: 'static_pages#events'
+  get 'gallery', to: 'static_pages#gallery'
+  get 'joinourteam', to: 'static_pages#joinourteam'
+  get 'donate', to: 'static_pages#donate'
 end
